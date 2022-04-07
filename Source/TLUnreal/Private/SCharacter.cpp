5b97db3272a49e6317c9d45d7e54a1c671e41f48
CristiCojocaru <cristicojocaru@mpg.io>
2022-04-07 16:35:57 +0100

    Assignment 1

--- Source/TLUnreal/Private/SCharacter.cpp
+++ Source/TLUnreal/Private/SCharacter.cpp
@@ -4,6 +4,7 @@
 #include "SCharacter.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
 // Sets default values
 ASCharacter::ASCharacter()
@@ -13,9 +14,13 @@ ASCharacter::ASCharacter()
 
 	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>("SpringArmComponent");
 	SpringArmComp->SetupAttachment(RootComponent);
+	SpringArmComp->bUsePawnControlRotation = true;
 
 	CameraComp = CreateDefaultSubobject<UCameraComponent>("CameraComponent");
 	CameraComp->SetupAttachment(SpringArmComp);
+
+	bUseControllerRotationYaw = false;
+	GetCharacterMovement()->bOrientRotationToMovement = true;
 }
 
 // Called when the game starts or when spawned
@@ -32,16 +37,6 @@ void ASCharacter::Tick(float DeltaTime)
 
 }
 
-void ASCharacter::MoveForward(float value)
-{
-	AddMovementInput(GetActorForwardVector(), value);
-}
-
-void ASCharacter::StrafeRight(float value)
-{
-	AddMovementInput(GetActorRightVector(), value);
-}
-
 // Called to bind functionality to input
 void ASCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
@@ -49,6 +44,59 @@ void ASCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponen
 
 	PlayerInputComponent->BindAxis("MoveForward", this, &ASCharacter::MoveForward);
 	PlayerInputComponent->BindAxis("StrafeRight", this, &ASCharacter::StrafeRight);
-	PlayerInputComponent->BindAxis("TurnRight", this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookUp", this, &ASCharacter::LookUp);
+	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+
+	PlayerInputComponent->BindAction("PrimaryAttack", IE_Pressed, this, &ASCharacter::PrimaryAttack);
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
 }
 
+void ASCharacter::MoveForward(float value)
+{
+	FRotator ControlRot = GetControlRotation();
+	ControlRot.Pitch = 0;
+	ControlRot.Roll = 0;
+
+	AddMovementInput(ControlRot.Vector(), value);
+}
+
+void ASCharacter::StrafeRight(float value)
+{
+	FRotator ControlRot = GetControlRotation();
+	ControlRot.Pitch = 0;
+	ControlRot.Roll = 0;
+
+	FVector RightVector = FRotationMatrix(ControlRot).GetScaledAxis(EAxis::Y);
+	AddMovementInput(RightVector, value);
+}
+
+void ASCharacter::LookUp(float value)
+{
+	float PitchRot = value;
+	float ControlRotPitch = GetControlRotation().Pitch;
+
+	if (ControlRotPitch + value < MaxPitchRotationUpper && ControlRotPitch + value > MaxPitchRotationLower)
+		if(ControlRotPitch + value > 180.0f)
+			PitchRot = ControlRotPitch - MaxPitchRotationUpper;
+		else
+			PitchRot = ControlRotPitch - MaxPitchRotationLower;
+
+	APawn::AddControllerPitchInput(PitchRot);
+}
+
+void ASCharacter::PrimaryAttack()
+{
+	//FRotator CameraRotation = GetControlRotation();
+	//CameraRotation.Pitch = 0;
+	//CameraRotation.Roll = 0;
+	//SetActorRotation(CameraRotation);
+
+	FVector HandLocation = GetMesh()->GetSocketLocation("Muzzle_01");
+	HandLocation += GetActorForwardVector() * ProjectileXDeviation;
+	FTransform SpawnTM(GetActorRotation(), HandLocation);
+
+	FActorSpawnParameters SpawnParams;
+	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+	GetWorld()->SpawnActor<AActor>(MainAttackProjectile, SpawnTM, SpawnParams);
+}
